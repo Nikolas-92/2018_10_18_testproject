92c518f152756b7a10a31cd215bbf8402b6830d6
Nikolas <Nikolas-92@yandex.ru>
2018-10-21 21:27:32 +0300

    First page done

---
+++ js/style.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // home.png
+  var a = document.querySelector(".no_lpadding");
+  a.onmouseenter = () => {
+    a.childNodes[1].src = "./img/home_hover.png";
+  }
+  a.onmouseleave = () => {
+    a.childNodes[1].src = "./img/home.png";
+  }
+
+
+  // cart.png
+  var cart_div = document.querySelector(".cart");
+  var bundle = [];
+
+  bundle[0] = cart_div.childNodes[1];
+  bundle[1] = cart_div.childNodes[3];
+  bundle[2] = cart_div.childNodes[5];
+  bundle[3] = cart_div.childNodes[7];
+
+  bundle[0].onmouseenter = () => {
+    bundle[1].style.backgroundColor = "#f13d70";
+  }
+  bundle[0].onmouseleave = () => {
+    bundle[1].style.backgroundColor = "#b8bfc0";
+  }
+
+  bundle[2].onmouseenter = () => {
+    bundle[3].style.backgroundColor = "#f13d70";
+  }
+  bundle[2].onmouseleave = () => {
+    bundle[3].style.backgroundColor = "#b8bfc0";
+  }
+});
